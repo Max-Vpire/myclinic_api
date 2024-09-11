9322995de0bafdc2a46423a1f4d0c26486eef153
Max-Vpire <olimjonmakhmudov26156@gmail.com>
2024-09-11 23:00:20 +0500

    all

--- src/doctors/doctors.service.ts
+++ src/doctors/doctors.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { DoctorsModel } from './doctors.model';
-import { Model } from 'sequelize';
+import { CreateDoctorDto } from './dto/create-doctor.dto';
+import { v4 } from 'uuid';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class DoctorsService {
@@ -9,7 +11,48 @@ export class DoctorsService {
         @InjectModel(DoctorsModel) private doctorsModel: typeof DoctorsModel
     ) {}
 
-    async create(dto) {
+    async create(dto: CreateDoctorDto) {
+        const hashedPasword = await bcrypt.hash(dto.password, 10)
+        const doctor = await this.doctorsModel.create({
+            ...dto,
+            id: v4(),
+            password: hashedPasword
+        })
+        .then(doctor => {
+            return doctor;
+        })
+        .catch(err => {
+            throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+        });
 
+        return doctor;
+    }
+
+    async findAll() {
+        const doctors = await this.doctorsModel.findAll({where: {deleted: false}})
+            .then(doctors => {
+                return doctors;
+            })
+            .catch(err => {
+                throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+            });
+
+            return doctors;
+    }
+
+    async findById(idx: string) {
+        const doctor = await this.doctorsModel.findOne({where: {id: idx, deleted: false}})
+            .then(doctor => {
+                return doctor;
+            })
+            .catch(err => {
+                throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+            });
+
+            if(!doctor) {
+                throw new NotFoundException();
+            }
+
+            return doctor;
     }
 }
